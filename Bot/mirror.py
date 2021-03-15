ecdb4ffa8296e04708029538abf8edf1c0c730d8
anonymous203030 <aram.simonyan.03@gmail.com>
2021-03-15 09:58:12 +0400

    Error Fixes

--- Bot/mirror.py
+++ Bot/mirror.py
@@ -5,27 +5,26 @@ from telethon.sync import TelegramClient
 import config
 from telethon import events
 
-
 bot = TelegramClient(StringSession(), config.API_ID, config.API_HASH).start(bot_token=config.BOT_TOKEN)
 client = TelegramClient(SQLiteSession(config.SESSION_STRING_CLIENT), config.API_ID, config.API_HASH)
 
-all = []
-file_path = "home/anonymous203030/Desktop/BotAz/Bot"
+file_path = os.path.abspath('Bot')
 file_name = os.path.basename(file_path)
 print('Starting Parsing:')
 for channel in config.SOURCE_CHANNEL:
     @client.on(events.NewMessage(chats=channel, incoming=True))
     async def handler_new_message(event):
-        global f
+        # global f
         try:
             print(event)
             print('\n\n\n\nStarting Parsing New Messages...\n...\n...\n.......\n')
-            if event.message.message != None:
-                #Sendind Parsed Message
+            f = await client.download_media(message=event.message)
+            if event.message.message is not None:
+                # Sending Parsed Message
                 try:
-                    f = await client.download_media(message=event.message)
                     await bot.send_message(config.TARGET_CHANNEL, event.message.message, file=f)
-                except:
+                except Exception as e:
+                    print(e)
                     await bot.send_message(config.TARGET_CHANNEL, event.message.message)
             else:
                 await bot.send_file(config.TARGET_CHANNEL, f)
